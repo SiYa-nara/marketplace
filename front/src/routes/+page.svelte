0c72b13b972810910183406e86d3b8e165ed6fa2
Simon Cornelius <simonjoecornelius@gmail.com>
2025-03-22 23:56:43 +0100

    feat: positioning the card around a bit
    also adjusting heading text

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,12 +1,33 @@
 <script>
-    import HoverCover from "$lib/components/hovercover.svelte";
+	import HoverCover from '$lib/components/hovercover.svelte';
 </script>
 
-<h1>marketplace</h1>
-<HoverCover src="/houseplant.png" alt="a nice, generic houseplant" coverColor="#628238" />
+<div class="hero">
+	<div class="the-center">
+		<h1>marketplace</h1>
+	</div>
+	<div id="houseplant">
+		<HoverCover src="/houseplant.png" alt="a nice, generic houseplant" coverColor="#628238" />
+	</div>
+</div>
 
 <style>
+	.hero {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100vw;
+		height: 100vh;
+	}
+
 	h1 {
 		font-family: 'Jost';
+        font-size: 4.125rem;
+	}
+
+	#houseplant {
+		position: absolute;
+		top: 80px;
+		left: 120px;
 	}
 </style>
